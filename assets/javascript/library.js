7eca87e1bc99fd248de28e9d7ecba26f3f386825
you <you@yous-MacBook.home>
2018-04-23 18:41:24 -0400

    initial upload

---
+++ assets/javascript/library.js
@@ -0,0 +1,12 @@
+// Library.JS - Grammar Guru
+// Script for sidebar nav
+
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+}
